a0bb7f8e43aba17f3368c3edd29dc5a518bcc0af
seikosilvakomesu <seikosilvakomesu@gmail.com>
2023-12-01 01:18:10 -0500

    Creando Pantalla Menu

---
+++ app/src/main/java/com/example/applibreria/item_libro.kt
@@ -0,0 +1,11 @@
+package com.example.applibreria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class item_libro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_libro)
+    }
+}
