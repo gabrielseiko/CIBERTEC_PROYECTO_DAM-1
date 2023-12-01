97caf578f1ca34106576684d36f1b870d3549738
seikosilvakomesu <seikosilvakomesu@gmail.com>
2023-12-01 01:51:21 -0500

    Creando Activity Listado de Libros

---
+++ app/src/main/java/com/example/applibreria/ListadoLibros.kt
@@ -0,0 +1,11 @@
+package com.example.applibreria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoLibros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_libros)
+    }
+}
